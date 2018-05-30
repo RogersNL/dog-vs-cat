a558bd634c205df66c19499d0754119b35fc8923
Austin Barr and Nick Rogers <austin@>
2018-05-30 13:10:11 -0700

    Fix formatting and add messages.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
   $("button#dog").click(function() {
-    $("ul#boolean").prepend("<li>Hello!</li>");
+    $("ul#boolean").append("<li>Woof Woof!</li>");
+    $("ul#buster").append("<li>Meow what do you want?</li>");
   });
 
   $("button#cat").click(function() {
-    $("ul#buster").prepend("<li>Goodbye!</li>");
+    $("ul#buster").append("<li>Meow</li>");
+    $("ul#boolean").append("<li>Woof, I said boo LEEEN!</li>");
   });
 
   $("button#mystery").click(function() {
-    $("ul").prepend("<li>Stop copying me!</li>");
+    $("ul#buster").append("<li>Trump s0n</li>");
+    $("ul#boolean").append("<li>'Merica</li>");
   });
 });
